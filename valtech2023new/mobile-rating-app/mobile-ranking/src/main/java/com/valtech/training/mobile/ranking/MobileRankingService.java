a7a7beaf43594cf3239015a687f968d77e4fb9f2
Vijaylakshmi Kalasa <vijaylakshmi.kalasa@valtech.com>
2023-11-30 10:30:05 +0530

    Pushing all assignments

--- valtech2023new/mobile-rating-app/mobile-ranking/src/main/java/com/valtech/training/mobile/ranking/MobileRankingService.java
+++
@@ -1,8 +0,0 @@
-package com.valtech.training.mobile.ranking;
-
-import java.util.List;
-
-public interface MobileRankingService  {
-
-	public int rankMobile(String mobile );
-}
--- valtech2023new/mobile-rating-app/pattern-checker/src/main/java/com/valtech/training/pattern/checker/PatternCheckerService.java
+++
@@ -1,7 +0,0 @@
-package com.valtech.training.pattern.checker;
-
-import java.util.List;
-
-public interface PatternCheckerService {
-	public List<String> listPatterns(String mobile);
-}
